6a855f5ef19729037d9636b58ca039d6fb5a3cd9
mcbloch <git@mcbloch.dev>
2024-12-27 23:20:28 +0100

    add pretty starry parallax background using a shader, also add a freecam

---
+++ free_cam.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("ui_left"):
+		self.move_local_x(-10)
+	if Input.is_action_pressed("ui_right"):
+		self.move_local_x(10)
+	if Input.is_action_pressed("ui_down"):
+		self.move_local_y(10)
+	if Input.is_action_pressed("ui_up"):
+		self.move_local_y(-10)
