1372bf2f422d90a75019592bd37314b57c410a20
FKD13 <44001949+FKD13@users.noreply.github.com>
2024-12-28 17:48:53 +0100

    particlessss

--- bullet.gd
+++ bullet.gd
@@ -5,12 +5,8 @@ var _origin: RigidBody2D
 func launch(direction: float, force: int, origin: RigidBody2D):
 	_origin = origin
 	apply_central_force(force*Vector2(cos(direction), sin(direction)))
-
-func _ready() -> void:
-	contact_monitor = true
-	max_contacts_reported = 2
-	body_entered.connect(_on_collision)
 		
 func _on_collision(body):
 	if body != _origin:
-		body.queue_free()
+		body.get_node("Explosion").emitting=true
+		#body.queue_free()
